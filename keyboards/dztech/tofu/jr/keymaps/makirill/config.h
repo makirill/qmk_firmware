b89a907875717945ef7d6250d81ebf49ede765f7
Kirill Malgichev <makirill@gmail.com>
2023-06-15 11:25:02 -0400

    Minor updates

---
+++ keyboards/dztech/tofu/jr/keymaps/makirill/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+    #undef RGB_DISABLE_AFTER_TIMEOUT
+    #define RGB_DISABLE_AFTER_TIMEOUT 900000     // 15 minutes (15 * 60 * 1000ms)
+    // Effects
+    //#define RGB_MATRIX_FRAMEBUFFER_EFFECTS  // Heatmap, Rain
+
+    #undef RGB_MATRIX_DEFAULT_MODE
+    #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#endif // RGB_MATRIX_ENABLE
